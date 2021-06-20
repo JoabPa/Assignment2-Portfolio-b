61a764e4affb09cf4821dbe283d39edf4061ac3a
JoabPa <joabparedes.ec@gmail.com>
2021-06-19 21:05:06 -0400

    project added to MongoDB Atlas

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Secret = exports.HostName = exports.LocalURI = void 0;
+exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
 exports.LocalURI = "mongodb://localhost/clothing_store";
-exports.HostName = "localhost";
+exports.RemoteURI = "mongodb+srv://admin:joab140103@cluster-portfolio.i4mwr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+exports.HostName = "RemoteHost";
 exports.Secret = "someSecret";
 //# sourceMappingURL=db.js.map
